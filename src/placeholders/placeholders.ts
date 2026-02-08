5a7a80650a4cea2131919e0699c0a871985fd6a2
Alejandro Nava <Tochtlinavadev@gmail.com>
2026-02-08 14:17:48 -0600

    xD

--- src/placeholders/placeholders.ts
+++ src/placeholders/placeholders.ts
@@ -1,2 +1,4 @@
-export const PHOTO_1 = "../src/assets/img/placeholder1.png";
+import photo_1 from "../src/assets/img/placeholder1.png"
+
+export const PHOTO_1 = photo_1.src;
 export const LUGAR = "TERRAZA CALINDA";
