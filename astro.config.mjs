c9b604b705b23d31a6a35bc5c9e8c3faab5f8a85
Alejandro Nava <Tochtlinavadev@gmail.com>
2026-02-07 21:52:43 -0600

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tochtlinavadev.dpdns.org',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://tochtlinavadev.dpdns.org",
 });
